4fd46430d1ef4aaa2bec94c32e5367271a2d6709
likui628 <likui.it@gmail.com>
2022-02-08 15:00:19 +0800

    feat: add todo

--- src/App/NewInput/index.tsx
+++ src/App/NewInput/index.tsx
@@ -1,8 +1,31 @@
+import React, { createRef } from "react"
+import { useAppDispatch } from "../../hooks"
+import { addTodoAction, AppDispatch } from "../../store"
+
 function NewInput() {
+  const textInput: React.RefObject<HTMLInputElement> = createRef<HTMLInputElement>()
+  const dispatch: AppDispatch = useAppDispatch()
+
+  function addTodo(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (textInput.current === null) return
+
+    const val = textInput.current.value.trim()
+    if (e.key === "Enter" && val.length > 0) {
+      dispatch(addTodoAction(val))
+      
+      textInput.current.value = ""
+    }
+  }
+
   return (
     <header className="header">
       <h1>todos</h1>
-      <input className="new-todo" placeholder="What needs to be done?" />
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        ref={textInput}
+        onKeyPress={(e) => addTodo(e)}
+      />
     </header>
   )
 }
