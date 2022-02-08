7124b17c527335ed91182f0b976e6605f232475c
likui628 <likui.it@gmail.com>
2022-02-08 18:17:26 +0800

    feat: display todos

--- src/App/TodoList/Item/index.tsx
+++ src/App/TodoList/Item/index.tsx
@@ -1,9 +1,15 @@
-function Item() {
+import { Todo } from "../../../types"
+
+interface Props {
+  todo: Todo
+}
+
+const Item: React.FC<Props> = ({ todo }) => {
   return (
     <li className="todo">
       <div className="view">
         <input className="toggle" type="checkbox" />
-        <label>todo</label>
+        <label>{todo.bodyText}</label>
         <button className="destroy"></button>
       </div>
       <input className="edit" type="text" />
--- src/App/TodoList/index.tsx
+++ src/App/TodoList/index.tsx
@@ -1,12 +1,21 @@
+import { ReactElement } from 'react'
+import { useAppSelector } from '../../hooks'
+import { Todo } from '../../types'
 import Item from './Item'
 
 function TodoList() {
+  const state = useAppSelector((state) => state.todos)
+
   return (
     <section className="main">
       <input id="toggle-all" className="toggle-all" type="checkbox" />
       <label htmlFor="toggle-all"></label>
       <ul className="todo-list">
-        <Item />
+        {
+          state.map((t: Todo): ReactElement => {
+            return <Item key={t.id} todo={t} />
+          })
+        }
       </ul>
     </section>
   )
