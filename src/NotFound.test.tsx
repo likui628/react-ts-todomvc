34d9407143dc9d7edb43c4e8095ba141ae23ea1b
likui628 <likui.it@gmail.com>
2022-02-11 16:43:19 +0800

    test: NotFound

---
+++ src/NotFound.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import NotFound from "./NotFound";
+
+describe("NotFound", () => {
+    test("should render Page Not Found message", () => {
+        render(<NotFound />)
+        expect(screen.getByText('Page Not Found')).toBeInTheDocument()
+    })
+})
